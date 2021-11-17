35e1a4860722961d587cbf40506d173b0e1a2d9e
jcabrera37 <56944675+jcabrera37@users.noreply.github.com>
2021-11-17 15:41:28 -0600

    add insert user on db

--- sistema/registrarUsuario.php
+++ sistema/registrarUsuario.php
@@ -5,6 +5,42 @@ if (empty($_SESSION['active']))
 	{
 		header('location: ../');
 	}
+include "../conexionBD.php";
+
+if (!empty($_POST)) 
+{
+    if(empty($_POST['nombre']) || empty($_POST['usuario']) || empty($_POST['clave']) || empty($_POST['codserie']) || empty($_POST['acceso']) )
+    {
+        echo '<script type="text/javascript">
+        alert("Debe llenar los datos correspondientes");
+        </script>';
+    }else{
+        $nombre = $_POST['nombre'];
+        $usuario = $_POST['usuario'];
+        $clave = $_POST['clave'];
+        $codserie = $_POST['codserie'];
+        $acceso = $_POST['acceso'];
+        $rol = 2; //rol 1 administrador y rol 2 usuario
+        $estatus = 1; //activo 1 desactivado es 2
+
+        $query_insert = mysqli_query($conection,"INSERT INTO `usuarios` (`id_user`, `nombre`, `usuario`, `clave`, `codserie`, `accesoempresa`, `id_rol`, `estatus`) 
+                                                VALUES (NULL, '$nombre', '$usuario', '$clave', '$codserie', '$acceso', '$rol', '$estatus');");
+                                                echo $query_insert;
+                 if ($query_insert) 
+                 {
+                    echo '<script type="text/javascript">
+                    alert("Usuario creado correctamente!");
+                    </script>';
+                 }else{
+                    echo '<script type="text/javascript">
+                    alert("Error al crear el usuario");
+                    </script>';
+                 }
+
+    }
+}
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -40,22 +76,26 @@ if (empty($_SESSION['active']))
                 <p class="card-description">
                     Registrar
                 </p>
-                <form class="forms-sample">
+                <form class="forms-sample" method="post">
                 <div class="form-group">
-                    <label >Nombre de Usuario</label>
-                    <input type="text" class="form-control" id="exampleInputName1" placeholder="Usuario">
+                    <label >Nombre</label>
+                    <input type="text" class="form-control"  name="nombre" placeholder="Nombre">
+                </div>
+                <div class="form-group">
+                    <label >Usuario</label>
+                    <input type="text" class="form-control" name="usuario" placeholder="Usuario">
                 </div>
                 <div class="form-group">
                     <label>Clave</label>
-                    <input type="text" class="form-control" id="exampleInputName1" placeholder="Clave">
+                    <input type="text" class="form-control"  name="clave" placeholder="Clave">
                 </div>
                 <div class="form-group">
                     <label>Codigo Serie</label>
-                    <input type="text" class="form-control" id="codserie" placeholder="Codigo">
+                    <input type="text" class="form-control"  name="codserie" placeholder="Codigo">
                 </div>
                 <div class="form-group">
                     <label >Acceso a empresa</label>
-                    <input type="text" class="form-control" id="exampleInputName1" placeholder="Acceso">
+                    <input type="text" class="form-control"  name="acceso" placeholder="Acceso">
                 </div>
                 <button type="submit" class="btn btn-primary mr-2">Guardar</button>
                 <button class="btn btn-light">Cancelar</button>
