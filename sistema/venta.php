74cfff81f8841e5e1544975cc7c13862d9294787
jcabrera37 <56944675+jcabrera37@users.noreply.github.com>
2022-01-27 22:19:40 -0600

    add modal purchase

--- sistema/venta.php
+++ sistema/venta.php
@@ -21,6 +21,7 @@
 <?php include "../includes/includes.php"; ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"/>
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+    <link rel="stylesheet" href="../css/styleCalendar.css" type="text/css">
 </head>
 <body>
     <div class="container-scroller d-flex">
@@ -54,111 +55,133 @@
                                             <option value="SERIE">SERIE</option>
                                             <option value="APLICACION">APLICACION</option>
                                         </select>
-                                        <input type="text" name="TERMINO2" class="form-control" placeholder="Buscar" value=" " style="width: 100px;  padding: 7px;  padding: 7px; font-size: 10pt;">
+                                        <input type="text" name="TERMINO2" class="form-control" placeholder="Buscar" value="..." style="width: 100px;  padding: 7px;  padding: 7px; font-size: 10pt;">
                                         
                                         <center><button type="submit" class="btn btn-primary" style="width: 20px;"><i class="mdi mdi-search-web"></i></a></button></center>
-                                    </form>
-                                </div>
-                            </div>
+                                        
+                                        <div class="col-xs-12 col-sm-2 input-position text-rigth">
+                                        <div class="panel-body"><!-- INICIO MODAL -->
+                                            <div class="form-control">
+                                                <button href="#cart" title="" data-toggle="modal" class="mdi mdi-cart"  data-original-title="Venta" type="button"> 
+                                                </button>
+                                            </div>
 
-                            <div class="col-lg-14">
-                            <div class="card">
-                                <div class="card-body">
-                                <h4 class="card-title">Productos</h4>
-                                    <div class="table-responsive">
-                                        <table class="table table-striped table-bordered table-hover"> 
-                                            <thead>
-                                                <tr>
-                                                    <td>Agregar</td>
-                                                    <td>COD</td>
-                                                    <td>NOMBRE</td>
-                                                    <td>EXISTENCIA</td>
-                                                    <td>PRECIO</td>
-                                                    <td>MARCA</td>
-                                                    <td>SERIE</td>
-                                                    <td>APLICACIONES</td>
-                                                    
-                                                </tr>
-                                            </thead>
-                                            <?php
-                                                $query = mysqli_query($connectionTrans, "SELECT COD_PROD, NOMBRE, EXACTUAL, VENTAUNITARIO, MARCA, SERIE, APLICACIONES 
-                                                                                        FROM productos 
-                                                                                        WHERE ESTATUS = 1;");
-
-                                                $resultado = mysqli_num_rows($query);
-
-                                                if ($resultado > 0){
-                                                    while ($datos = mysqli_fetch_array($query)){
-                                            ?>
-                                            <tbody>
-                                                <tr>
-                                                    <td><a href="" style="font-size: 18pt;"><i class="mdi mdi-plus-box"></i></a></td>
-                                                    <td><?php echo $datos['COD_PROD']?></td>
-                                                    <td><?php echo $datos['NOMBRE']?></td>
-                                                    <td><?php echo $datos['EXACTUAL']?></td>
-                                                    <td><?php echo $datos['VENTAUNITARIO']?></td>
-                                                    <td><?php echo $datos['MARCA']?></td>
-                                                    <td><?php echo $datos['SERIE']?></td>
-                                                    <td><?php echo $datos['APLICACIONES']?></td>
-                                                    
-                                                </tr>
-                                            <?php }
-                                            }?>
-                                            </tbody>
-                                        </table>
+                                            <div id="cart" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
+                                                <div class="modal-dialog">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            
+                                                            <h3 id="myModalLabel" align="center" >Productos</h3>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
+                                                        </div>
+                                                        
+                                                        <div class="modal-body">
+                                                            <div class="table-responsive">
+                                                                <h3>Nombre: Cliente test</h3>
+                                                                <h3>Dirección: Ciudad</h3>
+                                                                <h3>Nit: 32342342</h3>
+                                                                <table class="table table-striped table-bordered table-hover"> 
+                                                                    <thead>
+                                                                        <tr>
+                                                                            <td>COD</td>
+                                                                            <td>PROD</td>
+                                                                            <td>CTDAD</td>
+                                                                            <td>PRECIO</td>
+                                                                            <td>% DESC.</td>
+                                                                            <td>DESC.</td>
+                                                                            <td>SUB-TOTAL</td>
+                                                                        </tr>
+                                                                    </thead>
+                                                                    <tbody>
+                                                                        <tr>
+                                                                            <td>001002</td>
+                                                                            <td>ACEITE 20W50</td>
+                                                                            <td>1</td>
+                                                                            <td>170.00</td>
+                                                                            <td>5</td>
+                                                                            <td>7</td>
+                                                                            <td>165.00</td>
+                                                                        </tr>
+                                                                    </tbody>
+                                                                </table>
+                                                            
+                                                                <div class="row">
+                                                                    <div class="col-md-6"><h4>IVA </h4></div>
+                                                                    <div class="col-md-6 text-right">Q. 19.80</div>
+                                                                </div>
+                                                                <div class="row">
+                                                                    <div class="col-md-6"><h4>Sub-Total </h4></div>
+                                                                    <div class="col-md-6 text-right">Q. 145.20</div>
+                                                                </div>
+                                                                <div class="row">
+                                                                    <div class="col-md-6"><h3>TOTAL </h3></div>
+                                                                    <div class="col-md-6 text-right"><H4>Q. 165.00</H4></div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button name="btnfactua" href="facturar.php" type="button" class="btn btn-primary"><strong><a href="facturar.php" class="a-button">Facturar</a></strong></button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div><!-- FIN MODAL --> 
                                     </div>
-                                </div>
-                            </div>
-                        </div>
-                        </div>
-                        <!--
-                        <div class="col-lg-6">
-                            <div class="card">
-                                <div class="card-body">
-                                <h4 class="card-title">Productos a Facturar</h4>
-                                    <div class="table-responsive">
-                                        <table class="table table-striped table-bordered table-hover"> 
-                                            <thead>
-                                                <tr>
-                                                    <td>COD</td>
-                                                    <td>PROD</td>
-                                                    <td>CTDAD</td>
-                                                    <td>PRECIO</td>
-                                                    <td>% DESC.</td>
-                                                    <td>DESC.</td>
-                                                    <td>SUB-TOTAL</td>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr>
-                                                    <td>001002</td>
-                                                    <td>ACEITE 20W50</td>
-                                                    <td>1</td>
-                                                    <td>170.00</td>
-                                                    <td>5</td>
-                                                    <td>7</td>
-                                                    <td>165.00</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
                                        
-                                        <div class="row">
-                                            <div class="col-md-6"><h4>IVA </h4></div>
-                                            <div class="col-md-6 text-right">Q. 19.80</div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6"><h4>Sub-Total </h4></div>
-                                            <div class="col-md-6 text-right">Q. 145.20</div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-6"><h3>TOTAL </h3></div>
-                                            <div class="col-md-6 text-right"><H4>Q. 165.00</H4></div>
+                                    </form>
+                                    
+                                </div>
+                            </div>
+                            <div class="col-lg-14">
+                                <div class="card">
+                                    <div class="card-body">
+                                    <h4 class="card-title">Productos</h4>
+                                        <div class="table-responsive">
+                                            <table class="table table-striped table-bordered table-hover"> 
+                                                <thead>
+                                                    <tr>
+                                                        <td>Agregar</td>
+                                                        <td>COD</td>
+                                                        <td>NOMBRE</td>
+                                                        <td>EXISTENCIA</td>
+                                                        <td>PRECIO</td>
+                                                        <td>MARCA</td>
+                                                        <td>SERIE</td>
+                                                        <td>APLICACIONES</td>
+                                                        
+                                                    </tr>
+                                                </thead>
+                                                <?php
+                                                    $query = mysqli_query($connectionTrans, "SELECT COD_PROD, NOMBRE, EXACTUAL, VENTAUNITARIO, MARCA, SERIE, APLICACIONES 
+                                                                                            FROM productos 
+                                                                                            WHERE ESTATUS = 1;");
+
+                                                    $resultado = mysqli_num_rows($query);
+
+                                                    if ($resultado > 0){
+                                                        while ($datos = mysqli_fetch_array($query)){
+                                                ?>
+                                                <tbody>
+                                                    <tr>
+                                                        <td><a href="" style="font-size: 18pt;"><i class="mdi mdi-plus-box"></i></a></td>
+                                                        <td><?php echo $datos['COD_PROD']?></td>
+                                                        <td><?php echo $datos['NOMBRE']?></td>
+                                                        <td><?php echo $datos['EXACTUAL']?></td>
+                                                        <td><?php echo $datos['VENTAUNITARIO']?></td>
+                                                        <td><?php echo $datos['MARCA']?></td>
+                                                        <td><?php echo $datos['SERIE']?></td>
+                                                        <td><?php echo $datos['APLICACIONES']?></td>
+                                                        
+                                                    </tr>
+                                                <?php }
+                                                }?>
+                                                </tbody>
+                                            </table>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                                        -->
                     </div>
                 </div>
             </div>
